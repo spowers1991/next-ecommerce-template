d80bb9dbb1fd416485489eae231ddd404a5e265c
spowers1991 <stevenpowers1991@gmail.com>
2024-06-28 22:39:45 +0100

    feat(filter): added a filter context and functions for filtering products

--- components/products/widgets/FilterWidget.jsx
+++ components/products/widgets/FilterWidget.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
-import CheckboxSelector from '@/components/products/filter/selectors/CheckboxSelector';
-import FilterSubmit from '../filter/actions/FilterSubmit';
+import CheckboxSelector from '@/components/filter/selectors/CheckboxSelector';
+import FilterSubmit from '@/components/filter/actions/FilterSubmit';
 
 const FilterWidget = () => {
 
   return (
     <div className="bg-white border p-4 flex flex-col gap-y-4 items-start">
       <CheckboxSelector propertyName={'collections'} />
+      <CheckboxSelector propertyName={'variants'} />
     </div>
   );
 };
